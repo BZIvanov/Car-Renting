781fe391768538d915ce65a92de301d4e6945e84
BZIvanov <ivanov.biser@gmx.com>
2020-05-02 00:19:14 +0300

    add morgan and serving static files option

--- back-end/app.js
+++ back-end/app.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const cors = require('cors');
+const morgan = require('morgan');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const userRoutes = require('./routes/userRoutes');
@@ -7,9 +8,15 @@ const carRoutes = require('./routes/carRoutes');
 
 const app = express();
 
+if (process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
+
 app.use(cors());
 
 app.use(express.json({ limit: '10kb' }));
+
+app.use(express.static(`${__dirname}/public`));
 
 app.use('/api/user', userRoutes);
 app.use('/api/cars', carRoutes);
