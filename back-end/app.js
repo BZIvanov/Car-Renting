a8617bb4df33d001ab2c5a473d76b1d066342194
BZIvanov <ivanov.biser@gmx.com>
2020-05-05 20:31:04 +0300

    added security modules

--- back-end/app.js
+++ back-end/app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const cors = require('cors');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const mongoSanitize = require('express-mongo-sanitize');
+const xss = require('xss-clean');
 const globalErrorHandler = require('./controllers/errorController');
 const AppError = require('./utils/appError');
 const userRoutes = require('./routes/userRoutes');
@@ -23,6 +25,10 @@ app.use('/api', limiter);
 app.use(cors());
 
 app.use(express.json({ limit: '10kb' }));
+
+app.use(mongoSanitize());
+
+app.use(xss());
 
 app.use(express.static(`${__dirname}/public`));
 
