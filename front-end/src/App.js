454bbfc50ce5dbc94b1ebc0518e1dc0726fde870
BZIvanov <ivanov.biser@gmx.com>
2020-05-09 08:40:00 +0300

    moved routes in auth variable in case the user type the url directly in the browser

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { connect } from 'react-redux';
 import { BrowserRouter, Switch, Route } from 'react-router-dom';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import {
@@ -12,28 +13,46 @@ import {
 import { Layout, Logout } from './components/organisms';
 import * as constants from './constants';
 
-function App() {
+function App({ isAuthenticated }) {
+  let authRoutes = (
+    <Switch>
+      <Route path={constants.ROOT_PATH} exact component={Home} />
+      <Route
+        path={constants.ALL_AVAILABLE_CARS_PATH}
+        component={AvailableCars}
+      />
+      <Route path={constants.SIGNUP_PATH} component={Register} />
+      <Route path={constants.LOGIN_PATH} component={Login} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+
+  if (isAuthenticated) {
+    authRoutes = (
+      <Switch>
+        <Route path={constants.ROOT_PATH} exact component={Home} />
+        <Route
+          path={constants.ALL_AVAILABLE_CARS_PATH}
+          component={AvailableCars}
+        />
+        <Route path={constants.ADD_NEW_CAR_PATH} component={AddNewCar} />
+        <Route path={constants.LOGOUT_PATH} component={Logout} />
+        <Route component={NotFound} />
+      </Switch>
+    );
+  }
   return (
-    <>
-      <BrowserRouter>
-        <CssBaseline />
-        <Layout>
-          <Switch>
-            <Route path={constants.ROOT_PATH} exact component={Home} />
-            <Route
-              path={constants.ALL_AVAILABLE_CARS_PATH}
-              component={AvailableCars}
-            />
-            <Route path={constants.ADD_NEW_CAR_PATH} component={AddNewCar} />
-            <Route path={constants.SIGNUP_PATH} component={Register} />
-            <Route path={constants.LOGIN_PATH} component={Login} />
-            <Route path={constants.LOGOUT_PATH} component={Logout} />
-            <Route component={NotFound} />
-          </Switch>
-        </Layout>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <CssBaseline />
+      <Layout>{authRoutes}</Layout>
+    </BrowserRouter>
   );
 }
 
-export default App;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.users.userId !== null,
+  };
+};
+
+export default connect(mapStateToProps)(App);
