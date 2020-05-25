e7230ebe82dbc74ab925a415ca425f057be20a88
BZIvanov <ivanov.biser@gmx.com>
2020-05-25 19:46:29 +0300

    seeding cars data if no cars in the database

--- back-end/db/database.js
+++ back-end/db/database.js
@@ -1,4 +1,7 @@
+const fs = require('fs');
+const path = require('path');
 const mongoose = require('mongoose');
+const Car = require('../models/carModel');
 
 mongoose
   .connect(process.env.DB_PATH, {
@@ -12,3 +15,18 @@ mongoose
       `DB connection successful and ready for ${connection.connections[0].host}`
     );
   });
+
+mongoose.connection.once('connected', async (err) => {
+  if (err) {
+    return console.log(err);
+  }
+
+  const carCount = await Car.countDocuments();
+  if (carCount > 0) {
+    return;
+  }
+
+  const seedDataPath = path.join(__dirname, 'seedData.json');
+  const cars = fs.readFileSync(seedDataPath, 'utf8');
+  await Car.collection.insertMany(JSON.parse(cars));
+});
